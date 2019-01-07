ace7cf251240faef26837c6867676cbb43ff540b
tomdurrent <tomdurrent@sero.vip>
2019-01-07 13:36:00 +0800

    fix base58 convert bug

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'serocash:web3',
-  version: '0.2.16',
+  version: '0.2.19',
   summary: 'Sero JavaScript API, middleware to talk to a ethreum node over RPC',
   git: 'https://github.com/sero-cash/console',
   // By default, Meteor will default to using README.md for documentation.
