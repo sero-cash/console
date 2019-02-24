329b34bb69f55424149d06abe0f0c493a2d7bdfa
wincenteam <wincent@gameternal.com>
2019-02-24 20:04:34 +0800

    add method

---
+++ test/test_sero/web3.sero.getBlockReward.js
@@ -0,0 +1,7 @@
+var Web3 = require("../../lib/web3");
+var provider = "http://127.0.0.1:8545"
+
+var web3 = new Web3(new Web3.providers.HttpProvider(provider));
+var balance = web3.sero.getBalance(web3.sero.accounts[0]);
+console.log(balance)
+
